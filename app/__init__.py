1627d027a953eb61b537b7ec1f5d02195b5cf9a4
Alex <zaeho@mail.ru>
2018-12-29 01:40:22 +0400

    исправление ошибок

--- app/__init__.py
+++ app/__init__.py
@@ -10,42 +10,24 @@ from flask_migrate import Migrate
 from flask_login import LoginManager
 from flask_bootstrap import Bootstrap
 
-db = SQLAlchemy()
-migrate = Migrate()
-login = LoginManager()
-login.login_view = 'auth.login'
-login.login_message = ("Пожалуйста, войдите, чтобы открыть эту страницу.")
-bootstrap = Bootstrap()
+app = Flask(__name__)
+app.config.from_object(Config)
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+login = LoginManager(app)
+login.login_view = 'login'
+login.login_message = "Пожалуйста, войдите, чтобы открыть эту страницу."
+bootstrap = Bootstrap(app)
+
+from app import routes, models, errors
 
 # настройка поведения приложения в случае если отключен режим отладки
-def create_app(config_class=Config):
-	
-	app = Flask(__name__)
-	app.config.from_object(config_class)
-	db.init_app(app)
-	migrate.init_app(app, db)
-	login.init_app(app)	
-	bootstrap.init_app(app)
-	
-	if not app.debug and not app.testing:
-		
-		if app.config['LOG_TO_STDOUT']:
-			stream_handler = logging.StreamHandler()
-			stream_handler.setLevel(logging.INFO)
-			app.logger.addHandler(stream_handler)		
-		else:
-			if not os.path.exists('logs'):
-				os.mkdir('logs')
-			file_handler = RotatingFileHandler('logs/microblog.log', maxBytes=10240, backupCount=10)
-			file_handler.setFormatter(logging.Formatter(
-				'%(asctime)s %(levelname)s: %(message)s '
-				'[in %(pathname)s:%(lineno)d]'))
-			file_handler.setLevel(logging.INFO)
-			app.logger.addHandler(file_handler)
-		
-		app.logger.setLevel(logging.INFO)
-		app.logger.info('Microblog startup')
-	
-	return app
-	
-from app import models
+if not app.debug:
+	if not os.path.exists('logs'):
+		os.mkdir('logs')
+	file_handler = RotatingFileHandler('logs/microblog.log', maxBytes=10240, backupCount=10)
+	file_handler.setFormatter(logging.Formatter('%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]'))
+	file_handler.setLevel(logging.INFO)
+	app.logger.addHandler(file_handler)
+	app.logger.setLevel(logging.INFO)
+	app.logger.info('Microblog startup')
--- microblog.py
+++ microblog.py
@@ -1,9 +1,6 @@
 # Настройка оболочки приложения
-from app import create_app, db, cli
+from app import app, db
 from app.models import User, Post
-
-app = create_app()
-cli.register(app)
 
 # настройка контекста оболочки, который добавляет экземпляр и модели базы данных в сеанс оболочки
 @app.shell_context_processor
