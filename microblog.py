0d2bb83aa8114bf8ae1b9afcef7a6e2da172a990
Alex <zaeho@mail.ru>
2018-12-29 01:18:20 +0400

    исправление ошибок

--- microblog.py
+++ microblog.py
@@ -1,6 +1,9 @@
 # Настройка оболочки приложения
-from app import app, db
+from app import create_app, db, cli
 from app.models import User, Post
+
+app = create_app()
+cli.register(app)
 
 # настройка контекста оболочки, который добавляет экземпляр и модели базы данных в сеанс оболочки
 @app.shell_context_processor
